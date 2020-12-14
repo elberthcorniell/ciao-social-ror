ff4653158afe63ce2e856d59127e623232b4b077
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-14 17:43:34 -0400

    rubocop offenses fixed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,27 +1,26 @@
 class FriendshipsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-        @friends = current_user.friends
-        @requests = current_user.requests
-    end
+  def index
+    @friends = current_user.friends
+    @requests = current_user.requests
+  end
 
-    def create
-        @friendship = current_user.friendships.build(friend_id: params[:friend_id])
-        @friendship.save
-        redirect_to user_url(id: params[:friend_id])
-    end
+  def create
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    @friendship.save
+    redirect_to user_url(id: params[:friend_id])
+  end
 
-    def update
-        @friendship = Friendship.find(params[:id])
-        @friendship.update(status: true)
-        redirect_to friendships_path
-    end
+  def update
+    @friendship = Friendship.find(params[:id])
+    @friendship.update(status: true)
+    redirect_to friendships_path
+  end
 
-    def destroy
-        @friendship = Friendship.find(params[:id])
-        @friendship.destroy
-        redirect_to friendships_path
-    end
-
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    @friendship.destroy
+    redirect_to friendships_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Layout/LineLength
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -10,12 +12,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :friendships_2, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :friendships2, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
     Friendship.select("(CASE WHEN user_id = #{self[:id]} THEN friend_id ELSE user_id END) AS user_id, status").where(['(user_id = ? OR friend_id = ?)', self[:id], self[:id]])
   end
-  
+
   def requests
     Friendship.where(['friend_id = ? AND status IS null', self[:id]])
   end
@@ -24,7 +26,9 @@ class User < ApplicationRecord
     friend = Friendship.select(:status).where("friend_id = #{user_id} AND user_id = #{self[:id]}")
     puts "#{friend} klk"
     return false if friend.empty?
-    return friend.first.status
-  end
 
+    friend.first.status
+  end
 end
+
+# rubocop:enable Layout/LineLength
