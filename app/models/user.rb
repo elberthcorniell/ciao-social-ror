879b5aa7915b4cee55d2c3010b7618d7941b5b4e
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-14 15:36:37 -0400

    Table and Frienships controllers created

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :friendships_2, :class_name => "Friendship", :foreign_key => "friend_id"
+
+  def friends
+    Friendship.select("(CASE WHEN user_id = #{self[:id]} THEN friend_id ELSE user_id END) AS user_id").where(['user_id = ? OR friend_id = ?', self[:id], self[:id]])
+  end
+
 end
