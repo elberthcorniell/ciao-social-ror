b6e8f066f0c05b8ce196ae1de97be4244dc2ccf1
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-15 14:13:58 -0400

    Friend posts filter added

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,14 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :friendships2, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverted_friendships, class_name: "Friendship", foreign_key: "friend_id"
 
   def friends
     Friendship.select("(CASE WHEN user_id = #{self[:id]} THEN friend_id ELSE user_id END) AS user_id, status").where(['(user_id = ? OR friend_id = ?)', self[:id], self[:id]])
+  end
+
+  def friends_without_status
+    Friendship.select("(CASE WHEN user_id = #{self[:id]} THEN friend_id ELSE user_id END) AS user_id").where(['(user_id = ? OR friend_id = ?)', self[:id], self[:id]])
   end
 
   def requests
