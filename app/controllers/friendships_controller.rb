a303950472c28c2020b67ddd9ab343792e3a0cdb
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-16 12:12:47 -0400

    2 rows friendship creation added

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,19 +7,21 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id], status: true)
+    @friendship2 = User.find(params[:friend_id]).friendships.build(friend_id: current_user.id)
     @friendship.save
+    @friendship2.save
     redirect_to user_url(id: params[:friend_id])
   end
 
   def update
-    @friendship = Friendship.find(params[:id])
+    @friendship = Friendship.find([params[:id].to_i, current_user.id])
     @friendship.update(status: true)
     redirect_to friendships_path
   end
 
   def destroy
-    @friendship = Friendship.find(params[:id])
+    @friendship = Friendship.find([params[:id].to_i, current_user.id])
     @friendship.destroy
     redirect_to friendships_path
   end
